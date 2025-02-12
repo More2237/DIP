b46b42e2c40658904c18635ab17bebe864fb27da
More2237 <noorallyrahmonov2308@gmail.com>
2025-02-12 07:28:57 -0500

    12.02.2025

---
+++ ISP/Program.cs
@@ -0,0 +1,34 @@
+﻿// ISP
+public interface IUserCreationService
+{
+    void CreateUser(User user);
+    void DeleteUser(User user);
+}
+
+public interface IUserUpdateService
+{
+    void UpdateUser(User user);
+}
+
+public interface IUserQueryService
+{
+    User GetUser(int userId);
+}
+
+public class User
+{
+}
+
+public interface IUserNotificationService
+{
+    void SendEmail(User user, string message);
+}
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// public interface IUserService
+// {
+//     void CreateUser(User user);
+//     void DeleteUser(User user);
+//     void UpdateUser(User user);
+//     User GetUser(int userId);
+//     void SendEmail(User user, string message);
+// }
